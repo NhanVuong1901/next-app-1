3cad9ce62ed41cbd0d5943f6ca423d3d10c0e917
Vương Khánh Nhân <vuongkhanhnhan@gmail.com>
2026-02-02 16:00:18 +0700

    update delete

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { schema } from "../scheme";
+import { schema, schemaPatch } from "../scheme";
 import { prisma } from "@/prisma/lib/prisma";
 
 interface Props {
@@ -34,7 +34,16 @@ export async function PUT(request: NextRequest, { params }: Props) {
     return NextResponse.json(validation.error.issues, { status: 400 });
 
   const validData = validation.data;
-  return NextResponse.json({ id: 3, name: validData.name });
+
+  const updatedUser = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: {
+      name: validData.name,
+      email: validData.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser, { status: 200 });
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
@@ -47,5 +56,37 @@ export async function DELETE(request: NextRequest, { params }: Props) {
     return NextResponse.json({ message: "User Not Found" });
   }
 
+  await prisma.user.delete({
+    where: { id: parseInt(id) },
+  });
+
   return new NextResponse(null, { status: 204 });
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const { id } = await params;
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!user) {
+    return NextResponse.json({ message: "User Not Found" });
+  }
+  const body = await request.json();
+
+  const validation = schemaPatch.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.issues, { status: 400 });
+
+  const validData = validation.data;
+
+  const updatedUser = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: {
+      name: validData.name || user.name,
+      email: validData.email || user.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser, { status: 200 });
 }
--- app/api/users/scheme.ts
+++ app/api/users/scheme.ts
@@ -4,3 +4,8 @@ export const schema = z.object({
   name: z.string().trim().min(3),
   email: z.email().trim().toLowerCase(),
 });
+
+export const schemaPatch = z.object({
+  name: z.string().trim().min(3).optional(),
+  email: z.email().trim().toLowerCase().optional(),
+});
